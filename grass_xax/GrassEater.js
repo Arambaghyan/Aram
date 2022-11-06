ab9165abb4d96290437b3d3a500ce727bea8faf3
Miqayel <miqayel.tatikyan.y@tumo.org>
2022-11-06 14:32:46 +0400

    kisat jarangutyun

--- grass_xax/GrassEater.js
+++ grass_xax/GrassEater.js
@@ -1,12 +1,9 @@
-class GrassEater {
-  constructor(x, y, index) {
-    this.x = x;
-    this.y = y;
-    this.energy = 10;
-    this.multiply = 0;
-    this.index = index;
-    this.directions = [];
+class GrassEater extends LivingCreature {
+  constructor(x, y, index){
+      super(x, y, index);
+      this.energy = 8;
   }
+    
 
   getNewCoordinates() {
     this.directions = [
@@ -19,24 +16,6 @@ class GrassEater {
       [this.x, this.y + 1],
       [this.x + 1, this.y + 1]
     ];
-  }
-
-
-  chooseCell(character) {
-    this.getNewCoordinates();
-    var found = [];
-    for (var i in this.directions) {
-      var x = this.directions[i][0];
-      var y = this.directions[i][1];
-      if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-        if (matrix[x][y] == character) {
-          found.push(this.directions[i]);
-        }
-      }
-
-    }
-    return found;
-
   }
 
 
